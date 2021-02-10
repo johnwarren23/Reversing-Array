e48e4b0ceabdd7080fd25951e74ac8240556f7e0
johnwarren23 <78125600+johnwarren23@users.noreply.github.com>
2021-02-09 20:36:25 -0500

    Update ArrayReverse3.java

--- Reversing Array/src/ArrayReverse3.java
+++ Reversing Array/src/ArrayReverse3.java
@@ -8,9 +8,9 @@ public class ArrayReverse3
         Integer[] arr = {2, 5, 12, 31, 45};
         {
 
-            System.out.println(Arrays.asList(arr)); //prints out the original array
+            System.out.println(Arrays.asList(arr)); //prints out the original array [2, 5, 12, 31, 45]
             Collections.reverse(Arrays.asList(arr)); //this class reverses the array
-            System.out.println(Arrays.asList(arr)); //prints out the array in reversed order
+            System.out.println(Arrays.asList(arr)); //prints out the array in reversed order [45, 31, 12, 5, 2]
         }
     }
 }
